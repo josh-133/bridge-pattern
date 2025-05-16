b0e92db52708d6369954dd9171f53a8c06842dd9
hello world <joshuaburge507@gmail.com>
2025-05-16 15:32:11 +0930

    created a bridge pattern in an example to decouple shape and color hierarchies

---
+++ target/debug/bridge-pattern.d
@@ -0,0 +1,1 @@
+/Users/Joshua/GitHub/bridge-pattern/target/debug/bridge-pattern: /Users/Joshua/GitHub/bridge-pattern/src/main.rs
---
+++ target/debug/deps/bridge_pattern-84a1393447b4101f.d
@@ -0,0 +1,5 @@
+/Users/Joshua/GitHub/bridge-pattern/target/debug/deps/bridge_pattern-84a1393447b4101f: src/main.rs
+
+/Users/Joshua/GitHub/bridge-pattern/target/debug/deps/bridge_pattern-84a1393447b4101f.d: src/main.rs
+
+src/main.rs:
